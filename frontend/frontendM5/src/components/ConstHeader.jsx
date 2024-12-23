81313c0f5bbd536a3dd942afebb0ce836e1095ab
Rosanna Moss <rosannam@missionreadyhq.com>
2024-12-23 20:59:17 +1300

    progress on headers

--- frontend/frontendM5/src/components/ConstHeader.jsx
+++ frontend/frontendM5/src/components/ConstHeader.jsx
@@ -1,5 +1,69 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FaHeart, FaUser, FaSearch } from "react-icons/fa";
+import styles from "./ConstHeader.module.css";
+import Logo from "../assets/trademe-logo-no-tagline.png"
+import WatchlistIcon from "../assets/Group.png"
+import FavouritesIcon from "../assets/line-md_heart.png"
+import StartListingIcon from "../assets/prime_pencil.png"
+import MyTradeMeIcon from "../assets/lucide_circle-user-round.png"
 
 export default function ConstHeader() {
-  return <div></div>;
+  return (
+    <header className={styles.header}>
+    <div className={styles.topNav}>
+    <div className={styles.navLinks}>
+          <Link to="/">Trade Me</Link>
+          <Link to="/insurance">Trade Me Insurance</Link>
+          <Link to="/holiday">Holiday Houses</Link>
+          <Link to="/findsomeone">FindSomeone</Link>
+          <Link to="/motorweb">MotorWeb</Link>
+          <Link to="/homes">homes.co.nz</Link>
+        </div>
+        <div className={styles.authLinks}>
+    <Link to="/register">Register</Link>
+    <Link to="/login">Log in</Link>
+  </div>
+</div>
+
+<div className={styles.mainNav}>
+  <div className={styles.logo}>
+          <Link to="/">
+            <img src={Logo} alt="Trade Me" />
+          </Link>
+        </div>
+        
+        <div className={styles.searchSection}>
+        <div className={styles.browseDropdown}>
+            Browse
+            <span className={styles.dropdownArrow}>▼</span>
+</div>
+<div className={styles.searchInputContainer}>
+  <input type="text" placeholder="Search" />
+  <button className={styles.searchButton}>
+              <FaSearch />
+            </button>
+          </div>
+        </div>
+        
+        <div className={styles.userActions}>
+        <Link to="/watchlist" className={styles.actionLink}>
+            <span className={styles.icon}><img src={WatchlistIcon}></img></span>
+            Watchlist
+          </Link>
+          <Link to="/favourites" className={styles.actionLink}>
+ <span className={styles.icon}><img src={FavouritesIcon}></img></span>
+ Favourites
+</Link>
+<Link to="/listing/new" className={styles.actionLink}>
+<span className={styles.icon}><img src={StartListingIcon}></img></span>
+ Start a listing
+</Link>
+<Link to="/my-trademe" className={styles.actionLink}>
+My Trade Me <span className={styles.icon}><img src={MyTradeMeIcon}></img></span>
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
 }
